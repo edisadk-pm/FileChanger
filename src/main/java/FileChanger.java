05c051e5251eb65e218a2fc786728fd37609e4ec
Edi Sadikaj <sadikaj.edi@pm.me>
2026-03-04 09:58:28 +0100

    Add initial implementation of FileChanger and encoding configuration

---
+++ src/main/java/FileChanger.java
@@ -0,0 +1,9 @@
+public class FileChanger {
+
+    /*
+    your turn:
+    Write a program, which monitors the status of a file.
+    If the file is modified, print out some kind of message.
+    The file is located in resources and named demo.txt
+     */
+}
